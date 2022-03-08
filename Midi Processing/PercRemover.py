263e4d96f5bb6bf87108e86db50267321f35b007
Wouter Besse <lolmaker5@gmail.com>
2022-03-08 14:07:12 +0100

    Made it remove the percussion and write the rest to piano. Does result in some weird rhythm problems though

--- Midi Processing/PercRemover.py
+++ Midi Processing/PercRemover.py
@@ -1,34 +1,33 @@
-#from pathlib import Path
+# from pathlib import Path
 import mido
 from mido import MidiFile, MidiTrack
 
 # Import wanted midi file
-input_midi = MidiFile("H:/Projects/2021-2022/EMOPIA_cls-main/empty dataset/00e6d5785a99002bc645765b8851e9ff.mid", clip=True)
+input_midi = MidiFile("../../../EMOPIA_cls-main/empty dataset/00e6d5785a99002bc645765b8851e9ff.mid", clip=True)
 # Create output midi file
 output_midi = MidiFile()
 # Copy time metrics between both files
 output_midi.ticks_per_beat = input_midi.ticks_per_beat
+output_midi.type = input_midi.type
+# print(input_midi)
 
 for original_track in input_midi.tracks:
     new_track = MidiTrack()
-
+    #print(original_track)
     for msg in original_track:
-        if msg.type in ['note_on', 'note_off', 'control_change']:
-            origin_note = msg.note
+        if msg.type == 'note_on' or msg.type == 'note_off' or msg.type == 'control_change':
+            if msg.channel != 9:
+                new_track.append(msg.copy(channel = 2))
 
-            if origin_note in note_map:
-                new_track.append(
-                    msg.copy(note=note_map[origin_note]['target_note']))
-                print(note_map[origin_note]['description'])
-            else:
-                print("Origin note", origin_note, "not mapped")
         else:
-            print(msg.type)
             new_track.append(msg)
 
     # MIDI files are multitrack. Here we append
     # the new track with mapped notes to the output file
     output_midi.tracks.append(new_track)
+
+print(output_midi)
+output_midi.save('./Murundu-remap.mid')
 
 # def print_hi(name):
 #     # Use a breakpoint in the code line below to debug your script.
